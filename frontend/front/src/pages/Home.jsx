a65e668555496f095439642962ca50adbaaabb76
Luis <luiss18.code@gmail.com>
2025-02-10 21:24:04 -0300

    adicionado componente Recomendacao e integrado na página inicial

--- frontend/front/src/pages/Home.jsx
+++ frontend/front/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { Livro } from "../components/livro";
 import { Navigation } from "../components/navigation";
+import { Recomendacao } from "../components/recomendacao";
 
 import '../styles/Home.css'
 
@@ -11,7 +12,49 @@ import ImgLivro1 from '../assets/img/livro1.svg'
 import ImgLivro2 from '../assets/img/livro2.svg'
 import ImgLivro3 from '../assets/img/livro3.svg'
 
+import ImgPerfil from '../assets/img/perfil-test.svg'
+ 
 function Home () {
+
+    const recomendacoes = [
+        {
+          nome: "Haruka",
+          curso: "Analista de Sistemas",
+          texto: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          foto: ImgPerfil
+        },
+        {
+          nome: "Takashi",
+          curso: "Engenharia de Software",
+          texto: "Sempre admirei a dedicação da equipe. O aprendizado foi enriquecedor e a experiência prática fez toda a diferença.",
+          foto: ImgPerfil
+        },
+        {
+          nome: "Yuki",
+          curso: "Ciência da Computação",
+          texto: "O curso me ajudou a desenvolver habilidades essenciais para o mercado de trabalho. Recomendo para todos que querem crescer na área!",
+          foto: ImgPerfil
+        },
+        {
+          nome: "Souta",
+          curso: "Desenvolvimento Web",
+          texto: "A didática utilizada foi incrível! Aprendi muito sobre frameworks modernos e como aplicá-los em projetos reais.",
+          foto: ImgPerfil
+        },
+        {
+          nome: "Aoi",
+          curso: "Banco de Dados",
+          texto: "Um curso bem estruturado, com material de qualidade e professores que realmente entendem do assunto.",
+          foto: ImgPerfil
+        },
+        {
+          nome: "Ren",
+          curso: "Segurança da Informação",
+          texto: "Aprendi muito sobre cibersegurança e melhores práticas para proteger sistemas contra ataques.",
+          foto: ImgPerfil
+        }
+      ];
+
     return (
         <>
             <Header />
@@ -28,12 +71,27 @@ function Home () {
                 <button>Encontre outros títulos</button>
             </section>
 
-            {/* <section className="container-recomendacoes">
-                <h3>QUEM CONHECE RECOMENDA</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <section className="container-recomendacoes">
+                <div className="header-recomendacoes">
+                    <h3>QUEM CONHECE RECOMENDA</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+
+                    <div className="cards-recomendacoes">
+                        {recomendacoes.map((rec, index) => (
+                        <Recomendacao
+                            key={index}
+                            nome={rec.nome}
+                            curso={rec.curso}
+                            texto={rec.texto}
+                            foto={rec.foto}
+                        />
+                        ))}
+                    </div>
 
 
-            </section> */}
+
+            </section>
 
             <section className="container-editorial-section">
                 <div>
