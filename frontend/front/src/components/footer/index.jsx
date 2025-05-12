91384f7a757eb84992a98006f49a173ea6c79e8d
sougabrielxd <gabriellucasafb@gmail.com>
2025-05-12 15:44:05 -0300

    Mudança no Footer

--- frontend/front/src/components/footer/index.jsx
+++ frontend/front/src/components/footer/index.jsx
@@ -6,6 +6,7 @@ import WhatsApp2 from '../../assets/img/wpp.svg'
 import Linkedin from '../../assets/img/linkedin.svg'
 
 import {colors} from '../../colors'
+import { Link } from 'react-router-dom'
 
 import './style.css'
 
@@ -16,18 +17,6 @@ export function Footer () {
             <div className='footerContent'>
                 <div className='info'>
                     <img src={LogoFooter} alt="Logo Footer" className='Logo'/>
-                    <p>Juazeiro do Norte - CE <br /> CEP: 63000-0001
-                    </p>
-                    <div className='contact'>
-                        <img src={WhatsAppLogo} alt="WhatsApp Logo" />
-                        <p>(88) 97433-7571</p>
-                    </div>
-                    <figure>
-                        <img src={Instagram} alt="" />
-                        <img src={WhatsApp2} alt="" />
-                        <img src={Linkedin} alt="" />
-                        <img src={Facebook} alt="" />
-                    </figure>
                     
                 </div>
 
@@ -35,50 +24,68 @@ export function Footer () {
                     <div>
                         <h4>Navegação</h4>
                         <ul>
-                            <li>Home</li>
-                            <li>Institucional</li>
-                            <li>Estante</li>
-                            <li>Serviços</li>
-                            <li>Chamadas Abertas</li>
+                            <Link to={'/corpo-editorial'}>
+                            <li>Corpo Editorial</li>
+                            </Link>
+                            <Link to={'/codigo-de-etica'}>
+                            <li>Código de ética</li>
+                            </Link>
+                            <Link to={'/politica-de-privacidade'}>
+                            <li>Politica de Privacidade</li>
+                            </Link>
                         </ul>
                     </div>
 
                     <div>
-                        <h4>Navegação</h4>
+                        <h4>Ajuda</h4>
                         <ul>
-                            <li>Home</li>
-                            <li>Institucional</li>
-                            <li>Estante</li>
-                            <li>Serviços</li>
-                            <li>Chamadas Abertas</li>
+                        <Link to={'/corpo-editorial'}>
+                        <li>Fale conosco</li>
+                        </Link>
+                        <Link to={'/codigo-de-etica'}>
+                        <li>Status da plataforma</li>
+                        </Link>
+                        <Link to={'/politica-de-privacidade'}>
+                        <li>Reportar erro</li>
+                        </Link>
                         </ul>
                     </div>
 
                     <div>
-                        <h4>Navegação</h4>
+                        <h4>Links úteis</h4>
                         <ul>
-                            <li>Home</li>
-                            <li>Institucional</li>
-                            <li>Estante</li>
-                            <li>Serviços</li>
-                            <li>Chamadas Abertas</li>
+                        <Link to={'/corpo-editorial'}>
+                        <li>Termos de uso</li>
+                        </Link>
+                        <Link to={'/codigo-de-etica'}>
+                        <li>Estante</li>
+                        </Link>
+                        <Link to={'/politica-de-privacidade'}>
+                        <li>Termos de consentimento</li>
+                        </Link>
                         </ul>
                     </div>
 
                     <div>
-                        <h4>Navegação</h4>
+                        <h4>Contato</h4>
                         <ul>
-                            <li>Home</li>
-                            <li>Institucional</li>
-                            <li>Estante</li>
-                            <li>Serviços</li>
-                            <li>Chamadas Abertas</li>
+                        <div className='contact'>
+                        <img src={WhatsAppLogo} alt="WhatsApp Logo" />
+                        <li>(88) 97433-7571</li>
+                        </div>
                         </ul>
                     </div>
 
                 </div>
 
             </div>
+
+            <figure className='socialmedia'>
+                        <img src={Instagram} alt="" />
+                        <img src={WhatsApp2} alt="" />
+                        <img src={Linkedin} alt="" />
+                        <img src={Facebook} alt="" />
+                    </figure>
 
             <hr />
 
--- frontend/front/src/components/navigation/index.jsx
+++ frontend/front/src/components/navigation/index.jsx
@@ -18,29 +18,7 @@ export function Navigation () {
         <Link to={'/nossos-servicos'}>
           <li>Serviços</li>
         </Link>
-        <Link to={'/corpo-editorial'}>
-          <li>Corpo Editorial</li>
-        </Link>
-        <Link to={'/codigo-de-etica'}>
-          <li>Código de ética</li>
-        </Link>
-        <Link to={'/politica-de-privacidade'}>
-          <li>Politica de Privacidade</li>
-        </Link>
       </ul>
     </nav>
   );
 }
-
-/*
-Home🟩
-Institucional🟩
-Corpo Editorial 🟥
-Nossos Serviços 🟩
-Estante Virtual🟩
-Codigo de Etica 🟥
-Politica de Privacidade🟥
-Perguntas Frequêntes🟥
-
-Chamadas abertas 🟦 (Está no escopo geral do projeto, mas como na primeira versão do site não haverá essa aba, optamos por remove-la da navegation)
-*/
